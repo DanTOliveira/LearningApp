af5aaad506f0118cfa9d2fd2912a74eec798bcdc
Daniel Oliveira <danieltomas_3@hotmail.com>
2022-07-04 20:23:20 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Daniel Oliveira on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
